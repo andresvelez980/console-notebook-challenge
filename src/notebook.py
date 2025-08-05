9a26ba6e993b1275d161c39b33bcacce1ad8088b
andres velez <andrew.vm1611@gmail.com>
2025-08-04 19:53:02 -0500

    primer comit

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,12 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+HIGH: str = "HIGH"
+MEDIUM: str = "MEDIUM"
+LOW: str = "LOW"
+
+class Note:
+    def __init__(self, code: str, tittle: str, text: str, importance: str, creation_date: str):
+        self.code = code
+        self.tittle = tittle
+        self.text = text
+        self.importance = importance
+        self.creation_date = creation_date
