af1e3885bb4a34f6b5a5e0a4dae1881588f6eba3
Andres felipe <andrew.vm1611@gmail.com>
2026-02-16 17:24:29 -0500

    imlementacion del metodo "add_tag"

--- src/notebook.py
+++ src/notebook.py
@@ -1,15 +1,18 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
 from datetime import datetime
-datetime.now()
 
 class Note:
     HIGH: str = "HIGH"
     MEDIUM: str = "MEDIUM"
     LOW: str = "LOW"
-    def __init__(self, code: str, title: str, text: str, importance: str, tags: list[str]):
+    def __init__(self, code: str, title: str, text: str, importance: str,):
         self.code = code
         self.title = title
         self.text = text
         self.importance = importance
         self.creation_date = datetime.now()
-        self.tags = tags
+        self.tags = []
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
